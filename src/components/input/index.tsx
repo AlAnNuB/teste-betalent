198d3541c9e233371f06a07597368de76da64f85
Alan Miranda <alannub6@gmail.com>
2025-03-07 23:50:37 -0300

    feat: adiciona o componente input

---
+++ src/components/input/index.tsx
@@ -0,0 +1,25 @@
+import SearchIcon from '@/assets/search-svgrepo-com.svg';
+import './index.css';
+
+interface InputProps {
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+}
+
+export const Input = ({ value, onChange, placeholder = 'Pesquisar' }: InputProps) => {
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        placeholder={placeholder}
+        className="search-input"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      <span className="search-icon">
+        <img src={SearchIcon} alt="Ícone de busca" />
+      </span>
+    </div>
+  );
+}; 
