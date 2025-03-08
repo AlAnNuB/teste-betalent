01def4c1c73df9176a31657a497422b1914f4904
Alan Miranda <alannub6@gmail.com>
2025-03-08 00:00:16 -0300

    feat: adiciona o arquivo que inicializa a aplicação e o css global

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import "./main.css";
+import EmployeeDirectory from "./EmployeeDirectory/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<EmployeeDirectory />
 	</StrictMode>,
 );
