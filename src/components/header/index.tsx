cd1d758b8e9cdfeaafee4e63f6efc45e59c85f0d
Alan Miranda <alannub6@gmail.com>
2025-03-09 19:35:46 -0300

    feat: adicionando componente de header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+interface HeaderProps {
+  logo: string;
+  alt: string;
+}
+
+export const Header = ({ logo, alt }: HeaderProps) => {
+  return (
+    <header className="header">
+      <img src={logo} alt={alt} />
+    </header>
+  );
+};
